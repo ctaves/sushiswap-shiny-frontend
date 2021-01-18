bd54aa55282b6c761ce61aa0dea48ca81c38a48e
omakasebar <omakasebar@gmail.com>
2021-01-18 11:50:31 -0600

    update padding to Portfolio table

--- src/components/Portfolio/Tables/Farms.js
+++ src/components/Portfolio/Tables/Farms.js
@@ -14,7 +14,7 @@ const Table = ({ positions, farmBalanceUSD }) => {
         {/* Main column */}
         {/* Title */}
         <div className="flex flex-col w-0 flex-1 overflow-hidden">
-          <div className="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
+          <div className="bg-white px-4 py-5 border-b border-gray-200 sm:px-8">
             <div className="-ml-4 -mt-4 flex justify-between items-center flex-wrap sm:flex-no-wrap">
               <div className="ml-4 mt-4">
                 <h3 className="text-lg leading-6 font-medium text-gray-900">Farms</h3>
--- src/components/Portfolio/Tables/Sushi.js
+++ src/components/Portfolio/Tables/Sushi.js
@@ -9,7 +9,7 @@ const Table = ({ balances, price, total, SushiBalanceUSD, totalSushiBalance, tot
         {/* Main column */}
         {/* Title */}
         <div className="flex flex-col w-0 flex-1 overflow-hidden">
-          <div className="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
+          <div className="bg-white px-4 py-5 border-b border-gray-200 sm:px-8">
             <div className="-ml-4 -mt-4 flex justify-between items-center flex-wrap sm:flex-no-wrap">
               <div className="ml-4 mt-4">
                 <h3 className="text-lg leading-6 font-medium text-gray-900">Sushi Balance</h3>
