aae91f222ca10d57220a7216f8e6b8cde332a6bf
Matthew Lilley <hello@matthewlilley.com>
2021-01-21 07:27:32 +0000

    Remove analytics components (No need for these)

--- src/services/analytics/components/Difference.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Percent from "./Percent";
-
-export default function Difference(previousValue, currentValue) {
-  const value = ((currentValue - previousValue) / previousValue) * 100;
-  return <Percent value={value} />;
-}
