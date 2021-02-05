74ddd78c9abf9c862dee4c397951c71c3e692b77
omakasebar <omakasebar@gmail.com>
2021-02-04 23:00:53 -0600

    add unlock wallet illustration for farms

--- src/components/Farms/Table/AccountWrapper.js
+++ src/components/Farms/Table/AccountWrapper.js
@@ -11,7 +11,9 @@ const AccountWrapper = ({ children }) => {
           <div className="sushi-relative sushi-w-full sushi-mx-auto">
             <div className="sushi-grid gap-0 sushi-mx-auto sushi-grid-cols-1 md:sushi-grid-cols-3 lg:sushi-grid-cols-3 lg:sushi-max-w-none">
               <div className="sushi-col-span-1 md:sushi-col-span-2">{children}</div>
-              <div className="">{/* <UnlockWallet /> */}</div>
+              <div className="">
+                <UnlockWallet />
+              </div>
             </div>
           </div>
         </div>
