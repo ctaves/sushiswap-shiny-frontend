ce657078d9db68e174197ceec2c0c1ad92011d51
omakasebar <omakasebar@gmail.com>
2021-03-17 12:38:32 -0500

    readd default token list

--- src/services/exchange/constants/lists.ts
+++ src/services/exchange/constants/lists.ts
@@ -4,26 +4,28 @@
  */
 export const UNSUPPORTED_LIST_URLS: string[] = [];
 
-export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/sushiswapclassic/token-list/master/sushiswap.tokenlist.json';
+export const DEFAULT_TOKEN_LIST_URL =
+  "https://raw.githubusercontent.com/sushiswapclassic/token-list/master/sushiswap.tokenlist.json";
 
-const COMPOUND_LIST = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json';
-const UMA_LIST = 'https://umaproject.org/uma.tokenlist.json';
-const NFTX_LIST = 'https://nftx.ethereumdb.com/v1/tokenlist/';
-const AAVE_LIST = 'tokenlist.aave.eth';
-const SYNTHETIX_LIST = 'synths.snx.eth';
-const WRAPPED_LIST = 'wrapped.tokensoft.eth';
-const SET_LIST = 'https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json';
-const OPYN_LIST = 'https://raw.githubusercontent.com/opynfinance/opyn-tokenlist/master/opyn-v1.tokenlist.json';
-const ROLL_LIST = 'https://app.tryroll.com/tokens.json';
-const COINGECKO_LIST = 'https://tokens.coingecko.com/uniswap/all.json';
-const CMC_ALL_LIST = 'defi.cmc.eth';
-const CMC_STABLECOIN = 'stablecoin.cmc.eth';
-const KLEROS_LIST = 't2crtokens.eth';
-const GEMINI_LIST = 'https://www.gemini.com/uniswap/manifest.json';
-const QUICK_SWAP = 'https://unpkg.com/quickswap-default-token-list@1.0.39/build/quickswap-default.tokenlist.json';
+const COMPOUND_LIST = "https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json";
+const UMA_LIST = "https://umaproject.org/uma.tokenlist.json";
+const NFTX_LIST = "https://nftx.ethereumdb.com/v1/tokenlist/";
+const AAVE_LIST = "tokenlist.aave.eth";
+const SYNTHETIX_LIST = "synths.snx.eth";
+const WRAPPED_LIST = "wrapped.tokensoft.eth";
+const SET_LIST = "https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json";
+const OPYN_LIST = "https://raw.githubusercontent.com/opynfinance/opyn-tokenlist/master/opyn-v1.tokenlist.json";
+const ROLL_LIST = "https://app.tryroll.com/tokens.json";
+const COINGECKO_LIST = "https://tokens.coingecko.com/uniswap/all.json";
+const CMC_ALL_LIST = "defi.cmc.eth";
+const CMC_STABLECOIN = "stablecoin.cmc.eth";
+const KLEROS_LIST = "t2crtokens.eth";
+const GEMINI_LIST = "https://www.gemini.com/uniswap/manifest.json";
+const QUICK_SWAP = "https://unpkg.com/quickswap-default-token-list@1.0.39/build/quickswap-default.tokenlist.json";
 
 // lower index == higher priority for token import
 export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL,
   COMPOUND_LIST,
   AAVE_LIST,
   SYNTHETIX_LIST,
@@ -39,7 +41,7 @@ export const DEFAULT_LIST_OF_LISTS: string[] = [
   KLEROS_LIST,
   GEMINI_LIST,
   QUICK_SWAP,
-  ...UNSUPPORTED_LIST_URLS // need to load unsupported tokens as well
+  ...UNSUPPORTED_LIST_URLS, // need to load unsupported tokens as well
 ];
 
 // default lists to be 'active' aka searched across
