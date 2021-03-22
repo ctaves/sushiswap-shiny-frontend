252a0b1e78e3784f0b12dfba93bd269f9a5a04bb
0xJiro <jirosushi@protonmail.com>
2021-03-21 17:49:21 -0700

    Blacklist fake OHM token & pair

--- src/services/analytics/core/constants.js
+++ src/services/analytics/core/constants.js
@@ -3,9 +3,13 @@ export const TOKEN_DENY = [
   "0xc3761eb917cd790b30dad99f6cc5b4ff93c4f9ea",
   "0xe31debd7abff90b06bca21010dd860d8701fd901",
   "0xfc989fbb6b3024de5ca0144dc23c18a063942ac1",
+  "0x43f5e4ae7a5ed4c92ef99800d75bedac01665cea",
 ];
 
-export const PAIR_DENY = ["0xb6a741f37d6e455ebcc9f17e2c16d0586c3f57a5"];
+export const PAIR_DENY = [
+  "0xb6a741f37d6e455ebcc9f17e2c16d0586c3f57a5",
+  "0x33d2033ca58a66f039388e23cc906249b02b7006"
+];
 
 export const EXCHANGE_CREATED_TIMESTAMP = 1599214239;
 
